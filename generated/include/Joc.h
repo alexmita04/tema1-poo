9082603b42e4e6fc0aa08bf28073d484f2084373
alexmita04 <alexmita04@gmail.com>
2024-10-21 17:23:55 +0300

    added 2 functions as public in Joc.h

--- generated/include/Joc.h
+++ generated/include/Joc.h
@@ -21,16 +21,14 @@ class Joc
 public:
     Joc(const Scor &scorul_jocului_, const std::vector<Echipa> &echipe_, const std::vector<Jucator> &jucatori_);
     Joc(const std::string &fisier_echipe, const std::string &fisier_jucatori);
-
     friend std::ostream &operator<<(std::ostream &os, const Joc &joc);
-
     void start_game();
+    int cauta_jucator_in_jucatori_selectati(std::string nume_jucator);
+    int check_game_over();
 
 private:
     void descriere_joc();
     void amesteca_jucatori();
-    int cauta_jucator_in_jucatori_selectati(std::string nume_jucator);
-    int check_game_over();
     void tura_joc();
 };
 
